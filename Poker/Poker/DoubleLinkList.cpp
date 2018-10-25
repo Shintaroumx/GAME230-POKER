91c70003eb1a3886bc785cc050b1b44840f5c67b
Mx <shintarou@qq.com>
2018-10-24 19:27:23 -0700

    Ver1.2.4

--- Poker/Poker/DoubleLinkList.cpp
+++ Poker/Poker/DoubleLinkList.cpp
@@ -249,21 +249,21 @@ void DoubleLinkList::DeleteFromList(DoubleLinkList* list, DoubleLinkList* list2,
 	if (p->prev == NULL && p->next != NULL) {
 		list->head = p->next;
 		p->next->prev = NULL;
-		AddFirst(list2, p->data);
 		p->data.isKept = false;
+		AddFirst(list2, p->data);
 		delete p;
 	}
 	else if (p->prev == NULL && p->next == NULL) {
 		list->head = NULL;
-		AddFirst(list2, p->data);
 		p->data.isKept = false;
+		AddFirst(list2, p->data);
 		delete p;
 	}
 	else if (p->next != NULL) {
 		p->prev->next = p->next;
 		p->next->prev = p->prev;
-		AddFirst(list2, p->data);
 		p->data.isKept = false;
+		AddFirst(list2, p->data);
 		delete p;
 	}
 	else if (p->next == NULL) {
